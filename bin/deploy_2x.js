6bd44bf72faef56d583f6b44f9c84a413febbd95
edwingamboa <edwingamboa355@gmail.com>
2019-07-01 17:13:33 -0500

    Deploying documentation

---
+++ bin/deploy_2x.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+var shell = require('shelljs');
+
+function exec(command, afterCb) {
+shell.exec(command, function (code) {
+    console.log('Exit code:', code);
+    if (afterCb && typeof afterCb === 'function') {
+        afterCb();
+    }
+});
+}
+
+exec('npm run docs_2x:build', function () {
+    exec('git add --all && git commit -m "Deploying documentation" && git push', function () {
+        console.log('\x1b[32m%s\x1b[0m', 'Documentation (v2x) was deployed successfully');
+    });
+});
