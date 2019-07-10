e31e41f4f692efdea8f4de694f3f4d9aebbe9e59
edwingamboa <edwingamboa355@gmail.com>
2019-07-10 10:00:54 -0500

    Deploying documentation

--- bin/deploy_10.js
+++ bin/deploy_10.js
@@ -10,7 +10,7 @@ shell.exec(command, function (code) {
 });
 }
 
-exec('npm run docs_2x:build', function () {
+exec('npm run docs_10:build', function () {
     exec('git add --all && git commit -m "Deploying documentation" && git push', function () {
         console.log('\x1b[32m%s\x1b[0m', 'Documentation (Model Version <= 10.0.0) was deployed successfully');
     });
